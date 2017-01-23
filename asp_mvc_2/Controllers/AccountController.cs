3d9e77d5262cff4d62eaef69834a34e8660afe63
ridjalfathoni <fathonirosyidi@gmail.com>
2017-01-24 06:34:31 +0700

    ActionResult Login (Add)

--- asp_mvc_2/Controllers/AccountController.cs
+++ asp_mvc_2/Controllers/AccountController.cs
@@ -27,5 +27,34 @@ namespace Asp_mvc_2.Controllers
             }
             return View();
         }
+
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult LogIn(UserLoginView ULV, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                UserManager UM = new UserManager();
+                string password = UM.GetUserPassword(ULV.LoginName);
+                if (string.IsNullOrEmpty(password))
+                    ModelState.AddModelError("", "The user login or password provided is incorrect.");
+                else {
+                    if (ULV.Password.Equals(password))
+                    {
+                        FormsAuthentication.SetAuthCookie(ULV.LoginName, false);
+                        return RedirectToAction("Welcome", "Home");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "The password provided is incorrect.");
+                    }
+                }
+            }
+            // If we got this far, something failed, redisplay form
+            return View(ULV);
+        }
     }
 }
